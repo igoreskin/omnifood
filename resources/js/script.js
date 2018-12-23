9cd7d9f97ab94018818734b77e2d2714799dcfd5
Igor Eskin <eskinhd@gmail.com>
2018-12-23 16:30:42 -0500

    Navigation to different sections of the page is coded and animated

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // For the sticky navigation: 
     $('.js--section-features').waypoint(function(direction){
         if(direction == "down") {
             $('nav').addClass('sticky');
@@ -9,6 +10,37 @@ $(document).ready(function() {
     }, {
         offset: '60px;'
     })
+
+    // Scroll on buttons: the time-out is set to 0 because scroll-behavior is set to smooth in the css file
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 0);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 0);
+    });
+
+    // Navigation scroll: for some reason the 1000 ms time-out is not taken into account by this snippet 
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                && location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    // Animations on scroll: 
+    
+
 
     /*
     var waypoints = $('#handler-first').waypoint(function(direction) {
